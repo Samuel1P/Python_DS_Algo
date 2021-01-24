4b468381fbc97f19b5e4f0f0a04ea05296314e21
Samuel1P <ipsamuelip@gmail.com>
2021-01-24 22:02:51 +0530

    separated ds and algos from different repo

---
+++ algorithms/quick_sort_basic.py
@@ -0,0 +1,23 @@
+#Easy and best way to sort integers in a list
+#https://www.youtube.com/watch?v=kFeXwkgnQ9U
+
+arr_ = [3,2,0,1]
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[-1]
+    high = []
+    low = []
+    dup = []
+    for ele in arr:
+        if ele > pivot:
+            high.append(ele)
+        elif ele < pivot:
+            low.append(ele)
+        elif ele == pivot:
+            dup.append(ele)
+    return quick_sort(low)+dup+quick_sort(high)
+
+print (quick_sort(arr_))
+
