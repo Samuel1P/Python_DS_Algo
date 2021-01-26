8fee008530f27c80b5fae9396b0c471601813ec8
Samuel I P <ipsamuelip@gmail.com>
2021-01-26 22:09:04 +0530

    changed variable names to be explicit

--- data_structures/List/reverse_an_integer.py
+++ data_structures/List/reverse_an_integer.py
@@ -1,14 +1,14 @@
 num = 54321
 
 #initalize return value variable
-rev = 0
+reveresed_integer = 0
 # initialize remainder which will append (add) to the return value
-rem = 0
+remainder = 0
 
 while num > 0:
     # comments for iteration 1
-    rem = num % 10 # 1
+    remainder = num % 10 # 1
     num = num // 10 # 5432
-    rev = rev * 10 + rem # 0 * 10 + 1 = rev = 1
+    reveresed_integer = reveresed_integer * 10 + remainder # 0 * 10 + 1 = rev  = 1
 
-print (rev)
+print (reveresed_integer)
