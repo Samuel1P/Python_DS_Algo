9df7dae8d5ee0d6a9e73da20027959de3dc4df84
Samuel I P <ipsamuelip@gmail.com>
2022-05-18 21:45:07 +0530

    Rough Draft for Linked List

--- data_structures/Linked_List/Linked_List.py
+++ data_structures/Linked_List/Linked_List.py
@@ -2,8 +2,6 @@
 Linked List implimentation
 """
 from __future__ import annotations
-from tkinter.messagebox import NO
-
 class Node:
     """
     Node Class
@@ -19,7 +17,7 @@ class Node:
         """
         str represention of the node
         """
-        return f"Node('{self.value}') --points-to--> Node('{self.next}'))"
+        return f"Node('{self.value}') --points-to--> {self.next}"
         
 class LinkedList:
     """
@@ -49,6 +47,33 @@ class LinkedList:
         self.tail = node
         self.length += 1
     
+    def prepend(self, new_value):
+        print("prepend called...")
+        new_node = Node(new_value)
+        if not self.length:
+            print("Empty linked list.")
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    
+    def prepend(self, new_value):
+        print("prepend called...")
+        new_node = Node(new_value)
+        if not self.length:
+            print("Empty linked list.")
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
     def pop(self):
         print("pop called..")
         if not self.length:
@@ -67,43 +92,134 @@ class LinkedList:
             self.tail = None
         return temp_node
     
-    def prepend(self, new_value):
-        print("prepend called...")
-        new_node = Node(new_value)
+    def pop_first(self):
+        print("pop_first called..")
         if not self.length:
-            print("Empty linked list.")
-            self.head = new_node
-            self.tail = new_node
-            new_node.next = None
-        else:
-            new_node.next = self.head
-            self.head = new_node
-        self.length += 1
-
+            print("Empty linked list. Cannout Pop_First")
+            return None
+        temp_head = self.head
+        self.head = self.head.next
+        temp_head.next = None
+        self.length -= 1
+        if not self.length:
+            self.head = None
+            self.tail = None
+        return temp_head
+    
+    def get_node(self, index):
+        print(f"get index {index}")
+        if not self.length:
+            print("Linked list is EMPTY")
+            return None
+        if index < 0 or index > self.length-1:
+            print(f"Index {index} unavailable. Available index range till {len(self)-1}.")
+            return None
         
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_node(self, index, value):
+        print(f"set value {value} at index {index}...")
+        temp = self.get_node(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert_node(self, index, value):
+        new_node = Node(value)
+        print(f"insert {new_node} at index {index}...")
+        if index < 0 or index > self.length:
+            print(f"Index {index} unavailable. Available index range till {len(self)}.")
+            return False
+        if index == 0:
+            return self.prepend(new_node.value)
+        if index == len(self):
+            return self.append(new_node.value)
+        temp_node = self.get_node(index-1)
+        new_node.next = temp_node.next 
+        temp_node.next = new_node
+        self.length += 1
+        return True     
+    
+    def remove_node(self, index):
+        print(f"remove node at index {index}...")
+        if index < 0 or index >= self.length:
+            print(f"Index {index} unavailable. Available index range till {len(self)-1}.")
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == len(self)-1:
+            return self.pop()
+        temp_prev_node = self.get_node(index-1)
+        temp_node = temp_prev_node.next
+        temp_prev_node.next = temp_node.next 
+        temp_node.next = None
+        self.length -= 1
+        return temp_node
+    
+    def reverse(self):
+        after = self.head.next
+        self.head, self.tail = self.tail, self.head
+        temp = self.tail
+        before = None
+        for _ in range(len(self)):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+            
     def print_all(self):
-        print("Print all called :")
+        print(f"Print all called : {len(self)}")
         temp_node = self.head
-        while temp_node:
+        while temp_node: 
             print(f"Node : {temp_node.value}")
             temp_node = temp_node.next
     
     def __len__(self):
         return self.length
     
-LL1 = LinkedList("Abi")
-#LL2 = LinkedList()
+LL1 = LinkedList("Ash")
+# LL1 = LinkedList()
+LL2 = LinkedList()
 
-LL1.print_all()
 # LL2.print_all()
 
-LL1.prepend("Bob")
+# LL1.prepend("Bob")
+LL1.append("Bob")
 LL1.append("Charlie")
-# LL1.append("Charlie")
-LL1.prepend("Danny")
-# LL1.prepend("Danny")
+# LL1.remove_node(2)
 LL1.print_all()
+LL1.reverse()
+# LL1.print_all()
+# LL1.remove_node(0)
+LL1.print_all()
+LL1.reverse()
+# LL1.print_all()
+# LL1.remove_node(0)
+LL1.print_all()
+# exit()
+# LL1.print_all()
+
+# LL1.prepend("Danny")
+# LL1.prepend("Danny")
+# LL1.print_all()
 # LL2.append("Casper")
 # LL2.print_all()
-LL1.pop()
-LL1.print_all()
+# print(LL1.pop())
+# print(LL1.pop_first())
+# print(LL1.get_node(0))
+# print(LL1.set_node(0, 'Aaron'))
+# print(LL1.set_node(1, 'Barry'))
+# print(LL1.set_node(3, 'Cathy'))
+# print(LL1.get_node(1))
+# print(LL1.get_node(2))
+# print(LL1.get_node(3))
+# print(LL2.get_node(0))
+# print(LL1.insert_node(4, 'Barry'))
+
+
+
+# LL1.print_all()
